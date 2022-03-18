3cda451709507037d525732fe5ffef36c99a75a7
Abramov Trofim <EmperorTerra2000@yandex.ru>
2022-03-18 18:00:57 +0300

    feat: add hw-js-8

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,16 @@
 /**
  * генерация случайного числа
- * @param {number} number 
+ * @param {number} number
  * @returns {number}
  */
- export const getRandom = (number) => Math.ceil(Math.random() * number);
+export const getRandom = (number) => Math.ceil(Math.random() * number);
 
- /**
+/**
  * Возвращает время в текстовом формате
- * @param {number} date 
+ * @param {number} date
  * @returns {string}
  */
-export const getDateNormalType = (date) => date < 10 ? '0' + date : date;
+export const getDateNormalType = (date) => (date < 10 ? '0' + date : date);
 
 /**
  * Возвращает время
@@ -18,7 +18,52 @@ export const getDateNormalType = (date) => date < 10 ? '0' + date : date;
  */
 export const getDateNow = () => {
   const nowDate = new Date();
-  const textDate = `${getDateNormalType(nowDate.getHours())}:${getDateNormalType(nowDate.getMinutes())}:${getDateNormalType(nowDate.getSeconds())}`;
+  const textDate = `${getDateNormalType(nowDate.getHours())}:${getDateNormalType(
+    nowDate.getMinutes(),
+  )}:${getDateNormalType(nowDate.getSeconds())}`;
 
   return textDate;
-}
+};
+
+/**
+ * создание элемента документа
+ * @param {string} tag
+ * @param {string} className
+ * @returns {HTMLelement}
+ */
+export const createElement = (tag, className) => {
+  const $tag = document.createElement(tag);
+
+  if (className) {
+    $tag.classList.add(className);
+  }
+
+  return $tag;
+};
+
+/**
+ * создание button перезагрузки страницы
+ * @returns {HTMLelement}
+ */
+export const createReloadButton = () => {
+  const $reloadWrap = createElement('div', 'reloadWrap');
+  const $reloadButton = createElement('button', 'button');
+
+  handleClickReloadButton($reloadButton);
+
+  $reloadButton.innerText = 'Restart';
+
+  $reloadWrap.appendChild($reloadButton);
+
+  return $reloadWrap;
+};
+
+/**
+ * установка прослушки для баттона
+ * @param {HTMLelement} $reloadButton
+ */
+export const handleClickReloadButton = ($reloadButton) => {
+  $reloadButton.addEventListener('click', () => {
+    window.location.reload();
+  });
+};
